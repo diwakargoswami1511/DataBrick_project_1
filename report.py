0f674274d4cf0fbd26f8a8abfbbafd1d4ca9e9e8
diwakargoswami1511 <diwakar.goswami1511@gmail.com>
2026-04-17 12:59:08 +0530

    Add files via upload

---
+++ report.py
@@ -0,0 +1,33 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC **Best Model: Gradient Boosted Trees**
+# MAGIC
+# MAGIC Justification:
+# MAGIC
+# MAGIC - Highest ROC-AUC
+# MAGIC - Best Fraud Recall 
+# MAGIC - Balanced Precision
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **Business Impact Analysis:**
+# MAGIC
+# MAGIC If fraud = $1000 per case
+# MAGIC and we detect 92%, 
+# MAGIC we save millions yearly.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
